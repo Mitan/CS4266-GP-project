40d443d386d66e89b11dfc7db58c5eb27636ecdd
Mitan <dmitrykharkovsky@gmail.com>
2015-10-06 19:30:32 +0800

    add hypers infering to the model

--- FullGP_RBF.py
+++ FullGP_RBF.py
@@ -18,3 +18,16 @@ class FullGP_RBF():
 
     def predict(self, x_new):
         return self.model.predict(x_new)
+
+    def InferHypersHMC(self, number_of_samples):
+        self.model.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(1., 10.))
+        self.model.kern.variance.set_prior(GPy.priors.Gamma.from_EV(1., 10.))
+        self.model.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(1., 10.))
+        hmc = GPy.inference.mcmc.HMC(self.model, stepsize=5e-2)
+        s = hmc.sample(num_samples=number_of_samples)
+        cutoff_edge = number_of_samples / 3
+        samples = s[cutoff_edge:]  # cut out the burn-in period
+
+        self.model.kern.variance[:] = samples[:, 0].mean()
+        self.model.kern.lengthscale[:] = samples[:, 1].mean()
+        self.model.likelihood.variance[:] = samples[:, 2].mean()
